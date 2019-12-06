4afa036e7612beb27a732430407999a3940b611d
fe-shin <farid_ssh@yahoo.com>
2019-12-06 13:59:19 +0330

    Polishing a bit of menu and fonts and adding font-awesome icon in case of emergency (may delete later)

--- TEDx/assets/js/app.js
+++ TEDx/assets/js/app.js
@@ -1,13 +1,17 @@
 window.onscroll = function() {
   myFunction();
 };
+
 let navbar = document.querySelector(".stickyNavbar");
-let sticky = navbar.offsetTop;
+let changePoint = document.querySelector(".description").offsetTop;
+
 // let mainNav = document.querySelector(".navbar");
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset > changePoint) {
+    navbar.classList.remove("hidden");
     navbar.classList.add("sticky");
   } else {
+    navbar.classList.add("hidden");
     navbar.classList.remove("sticky");
   }
 }
