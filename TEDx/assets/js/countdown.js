93f2d16b64b688957b3b4a9786df3d04989aba3d
fe-shin <farid_ssh@yahoo.com>
2019-12-09 22:26:57 +0330

    TEDx v1.2 Fully Responsive

--- TEDx/assets/js/countdown.js
+++ TEDx/assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 9, 2019 19:23:00").getTime();
+var countDownDate = new Date("Dec 27, 2019 09:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
